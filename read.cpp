ee143d143615e680c644fcaf3a461d72846cb7b9
dahutchinson <dom@domhutchinson.me>
2020-01-24 16:11:26 +0000

    separated parsing edge

--- read.cpp
+++ read.cpp
@@ -1,43 +1,62 @@
 /*
  *  Read & print each edge from a file
  */
+
 #include <iostream>
 #include <fstream>
 #include <string>
 
 using namespace std;
 
-struct edge {
+/*---------*
+ * CLASSES *
+ *---------*/
+
+struct edge { // undirected edge
   int fst;
   int snd;
 };
 
-int main() {
+/*------------*
+ * SIGNATURES *
+ *------------*/
 
+void parse_edge(string str, edge& e);
+
+/*------*
+ * BODY *
+ *------*/
+
+int main() {
   ifstream stream("data/facebook_small.edges"); // File to read from
   string line; // current line being read
   string fst,snd;
   edge e;
 
   while (getline(stream,line)) { // Read each line
-    fst=""; snd=""; // initialise temp variables
-    bool after=false; // record whether after seperator (ie if we are reading first or second id)
-
-    for (char& c:line) { // read each character
-      if (c==' ') { // seperator
-        after=true;
-      } else if (after) { // second id
-        snd+=c;
-      } else { // first id
-        fst+=c;
-      }
-    }
-    // Create edge
-    e.fst=stoi(fst);
-    e.snd=stoi(snd);
-
+    parse_edge(line,e);
     cout<<e.fst<<" & "<<e.snd<<endl; // print
   }
 
   return 0;
+}
+
+// parse ege from stream
+void parse_edge(string str, edge& e) {
+  string fst="",snd="";
+  bool after=false;
+
+  for (char& c:str) {
+    if (c==' ') { // seperator
+      after=true;
+    } else if (after) { // second id
+      snd+=c;
+    } else { // first id
+      fst+=c;
+    }
+  }
+
+  // Update edge values
+  e.fst=stoi(fst);
+  e.snd=stoi(snd);
 }
