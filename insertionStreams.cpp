c51c5b42452c070a8a8076a0abf8b7e87c143a11
dahutchinson <dom@domhutchinson.me>
2020-01-25 14:45:55 +0000

    Now runs in single pass

--- insertionStreams.cpp
+++ insertionStreams.cpp
@@ -22,7 +22,7 @@ struct edge { // undirected edge
 *------------*/
 
 // size = size of resevoir
-void deg_res_sampling(int d1, int d2, int size, ifstream& stream, vector<int> &neighbourhood);
+void single_pass_insertion_stream(int c, int d, int size, ifstream& stream, vector<int> &neighbourhood);
 void update_resevoir(int node, int d1, int d2, int count, int size, vector<int>& resevoir, vector<edge>& edges);
 void parse_edge(string str, edge& e);
 
@@ -31,35 +31,33 @@ void parse_edge(string str, edge& e);
 *------*/
 
 int main() {
-  int c=100, d=400; // c=runs, d/c=d2
-  vector<int>* ptrs[c]; // pointers to each generated neighbourhood
-  vector<int> success; // indices of successes
+  int c=10, d=400, s=2; // c=runs, d/c=d2, s=size
+    ifstream stream("data/facebook.edges");
+    vector<int> n;
+    single_pass_insertion_stream(c,d,s,stream,n); // NB does not tell you whose neighbourhood it is
 
-  for (int i=0; i<c; i++) { // step d1 each iteration
-    ifstream stream("data/facebook.edges"); // load stream
-    vector<int>* n=new vector<int>; // generate vector for neighbourhood
-    ptrs[i]=n; // store pointer
-    deg_res_sampling(max(1,(i*d)/c),d/c,10,stream,*n); // NB does not tell you whose neighbourhood it is
-    if (n->size()==d/c) success.push_back(i); // record index if successful run
-  }
+    // Print out returned neighbourhood, if one exists
+    if (n.size()==0) {
+      cout<<"NO SUCCESSES"<<endl;
+    } else {
+      for (vector<int>::iterator i=n.begin(); i!=n.end(); i++) cout<<*i<<endl;
+    }
 
-  if (success.size()>0) { // if there were any successes
-    int i=rand()%success.size(); // generate index
-    vector<int>* n=ptrs[success[i]]; // pointer to neighbourhood
-    cout<<"RUN - "<<i<<"("<<max(1,(i*d)/c)<<","<<d/c<<")"<<endl; // details of runs
-    for (vector<int>::iterator i=n->begin(); i!=n->end(); i++) cout<<*i<<endl; // output neighbourhood
     return 0;
-  } else { // no successes
-    cout<<"NO SUCCESSES"<<endl;
-    return -1;
   }
-
-}
 
 // perform resevoir sampling
-void deg_res_sampling(int d1, int d2, int size, ifstream& stream, vector<int> &neighbourhood) {
-  string line; edge e; map<int,int> degrees; vector<edge> edges; vector<int> resevoir;
-  int count;  // number of nodes >=d1
+void single_pass_insertion_stream(int c, int d, int size, ifstream& stream, vector<int> &neighbourhood) {
+
+  // initalise edge & vector sets for each parallel run
+  vector<int>* resevoirs[c]; vector<edge>* edges[c];
+  for (int i=0; i<c; i++) {
+    resevoirs[i]=new vector<int>;
+    edges[i]=new vector<edge>;
+  }
+
+  string line; edge e; map<int,int> degrees; // these are shared for each run
+  int count[c];  // number of nodes >=d1
 
   while (getline(stream,line)) { // While stream is not empty
     parse_edge(line,e);
@@ -77,43 +75,53 @@ void deg_res_sampling(int d1, int d2, int size, ifstream& stream, vector<int> &n
       degrees[e.snd]=1;
     }
 
-    // Consider adding first node to the resevoir
-    if (degrees[e.fst]==d1) {
-      count+=1; // increment number of d1 degree nodes
-      update_resevoir(e.fst,d1,d2,count,size,resevoir,edges); // possibly add value to resevoir
-    }
+    for (int j=0; j<c; j++) { // perform parallel runs
+      int d1=max(1,(j*d)/c), d2=d/c; // calculate degree bounds for run
+      // NB rest is standard degree-restricted sampling
 
-    // Consider adding second node to the resevoir
-    if (degrees[e.snd]==d1) {
-      count+=1; // increment number of d1 degree nodes
-      update_resevoir(e.snd,d1,d2,count,size,resevoir,edges); // possibly add value to resevoir
-    }
+      // Consider adding first node to the resevoir
+      if (degrees[e.fst]==d1) {
+        count[j]+=1; // increment number of d1 degree nodes
+        update_resevoir(e.fst,d1,d2,count[j],size,*resevoirs[j],*edges[j]); // possibly add value to resevoir
+      }
 
-    // If one the endpoints is in the resevoir add the edge to collection of edges
-    if ((find(resevoir.begin(),resevoir.end(),e.fst)!=resevoir.end() && degrees[e.fst]<d2+d1)
-      || (find(resevoir.begin(),resevoir.end(),e.snd)!=resevoir.end() && degrees[e.snd]<d2+d1)) {
-      edges.push_back(e);
+      // Consider adding second node to the resevoir
+      if (degrees[e.snd]==d1) {
+        count[j]+=1; // increment number of d1 degree nodes
+        update_resevoir(e.snd,d1,d2,count[j],size,*resevoirs[j],*edges[j]); // possibly add value to resevoir
+      }
+
+      // If one the endpoints is in the resevoir add the edge to collection of edges
+      if ((find(resevoirs[j]->begin(),resevoirs[j]->end(),e.fst)!=resevoirs[j]->end() && degrees[e.fst]<d2+d1)
+        || (find(resevoirs[j]->begin(),resevoirs[j]->end(),e.snd)!=resevoirs[j]->end() && degrees[e.snd]<d2+d1)) {
+        edges[j]->push_back(e);
+      }
+
     }
 
   }
 
-  while (true) {
-    if (resevoir.size()==0) { // unsuccessful
-      neighbourhood.clear();
-      return;
+  // find successful neighbourhoods for each run
+  vector<tuple<int, int> > success; // (run, node_id)
+  for (int j=0; j<c; j++) { // cheack each run
+    int d1=max(1,(j*d)/c), d2=d/c; // calculate required degree bounds
+    for (vector<int>::iterator i=resevoirs[j]->begin(); i!=resevoirs[j]->end(); i++) {
+      if (degrees[*i]>=d1+d2) success.push_back(make_tuple(j,*i)); // record which nodes had sufficient degree in each run
     }
+  }
 
-    int x=rand()%resevoir.size(); // randomly choose a node
-    int node=resevoir[x];
-    if (degrees[node]>=d2+d1) { // check it has a sufficiently large neighbourhood
-      for (vector<edge>::iterator i=edges.begin(); i!=edges.end(); i++) { // construct & return the neighbourhood
-        if (i->fst==node) neighbourhood.push_back(i->snd);
-        if (i->snd==node) neighbourhood.push_back(i->fst);
-      }
-      return;
-    } else { // pick a different node
-      resevoir.erase(resevoir.begin()+x);
+  if (success.size()==0) { // No sucessful runs
+    neighbourhood.clear();
+    return
+  } else { // at least one successful run
+    int x=rand()%success.size(); // randomly choose a successful (run, node_id) pair
+    int node=get<1>(success[x]);
+    vector<edge>* edge_set=edges[get<0>(success[x])];
+    for (vector<edge>::iterator i=edge_set->begin(); i!=edge_set->end(); i++) { // construct neighbourhood to be returned
+      if (i->fst==node) neighbourhood.push_back(i->snd);
+      else if (i->snd==node) neighbourhood.push_back(i->fst);
     }
+    return
   }
 
 }
